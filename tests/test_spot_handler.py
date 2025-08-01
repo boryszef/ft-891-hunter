9e18a8283a24d54d7a35b976641bf802097a6ce4
Borys Szefczyk <boryszef@gmail.com>
2025-08-01 14:03:02 +0200

    Add distance test

--- tests/test_spot_handler.py
+++ tests/test_spot_handler.py
@@ -1,3 +1,4 @@
+import pytest
 from pydantic import BaseModel
 
 from ft_891_hunter.worker import SpotHandler
@@ -5,7 +6,7 @@ from ft_891_hunter.worker import SpotHandler
 
 def test_spot_handler():
     handler = SpotHandler()
-    with open('test/pota_response.json', encoding='utf-8') as pota_file:
+    with open('tests/pota_response.json', encoding='utf-8') as pota_file:
         handler.store_spots(('pota', pota_file.read()))
 
     assert len(handler.spots['pota']) == 3
@@ -13,3 +14,4 @@ def test_spot_handler():
     spot = handler.spots['pota'][0]
     assert isinstance(spot, BaseModel)
     assert spot.locator == 'JO87kb'
+    assert pytest.approx(spot.distance, 1) == 6520
